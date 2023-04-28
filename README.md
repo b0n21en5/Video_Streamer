fe45b211028e09a792286a7b899f863e95b33606
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-04-28 23:29:17 +0530

    documentation updated

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # <a href="https://video-streamer.onrender.com/" >Video Streaming Application</a>
 
 ![](https://github.com/b0n21en5/Video_Streamer/blob/main/o.gif)
+
+<hr>
+
+## Guide to run Locally
+
+### Step-1: Clone this repo in your system
+
+### Step 2: open terminal from server directory & enter 'npm i' this will install server dependencies
+
+### Step 3: open another terminal from client directory & enter 'npm i' this will install client dependencies
+
+### Step 4: set environment variable with your mongo-url, port & jwt-secret-key in .env file as shown in .env.example file
+
+### step 5: after all dependencies installed enter 'npm run dev' command in server directory terminal to run the application
--- client/src/utils/FetchFromAPI.js
+++ client/src/utils/FetchFromAPI.js
@@ -1,17 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+// you can add your own rapid api key below or in '.env' file
+const REACT_APP_RAPID_API_KEY =
+  "1b833ec468mshb55c1fb80c72afbp1dc25ejsncd2a60ae0551";
 
 const options = {
-    params: {maxResults: '50'},
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-  };
+  params: { maxResults: "50" },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
 
-  export const FetchFromAPI = async(url) =>{
-    const { data } = await axios.get(`${BASE_URL}/${url}`,options);
+export const FetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-    return data;
-  }
+  return data;
+};
